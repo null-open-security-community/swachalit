ec7a17bedef0eea7f0ae90f48c19ed72808a94b4
sunilkr <badboy16a@gmail.com>
2019-05-30 21:50:08 +0530

    Added negative tests

--- test/integration/leads_event_sessions_test.rb
+++ test/integration/leads_event_sessions_test.rb
@@ -70,4 +70,41 @@ class LeadsEventsSessionsTest < ActionDispatch::IntegrationTest
     get edit_leads_event_event_session_path(event, session)
     assert_response :ok
   end
+
+  test "Non lead user cannot create session" do
+    sign_in(users(:two))
+    event = events(:one)
+    speaker = users(:two)
+    session = {
+      event_id:     event.id,
+      user_id:      users(:two).id,
+      name:         "Event1::Session1", 
+      description:  "Session 1 for Event 1",
+      start_time:   event.start_time + 5.minutes,
+      end_time:     event.start_time + 30.minutes
+    }
+
+    post leads_event_event_sessions_path(event), event_session: session
+    assert_response(401)
+    
+    sess = EventSession.where(name: session[:name]).first
+    assert sess.nil?
+  end
+
+  test "Non lead user cannot update session" do
+    sign_in(users(:two))
+
+    session = event_sessions(:two)
+    event = events(:one)
+    update = {
+      start_time: session.start_time + 5.minutes,
+      end_time: session.end_time + 5.minutes
+    }
+
+    patch leads_event_event_session_path(event, session), event_session: update
+    assert_response(401)
+
+    s = EventSession.where(id: session.id).first
+    assert s.start_time != update[:start_time]
+  end
 end
