b9153464b6b475f928fb053f57884688367825b4
sunilkr <badboy16a@gmail.com>
2019-05-30 23:08:33 +0530

    Added test for event registration. Not working

---
+++ test/integration/event_registration_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class EventRegistrationsTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "Access event registration page" do
+    sign_in users(:two)
+
+    registration = event_registrations(:one)
+    event = events(:one)
+
+    get event_event_registrations_path(event)
+    assert_response :ok
+
+    get event_event_registration_path(event, registration)
+    assert_response :ok
+
+    get new_event_event_registration_path(event)
+    assert_response :ok
+
+    get edit_event_event_registration_path(event, registration)
+    assert_response :ok
+  end
+  
+  test "Non logged user should be redirected" do
+    event = events(:one)
+    
+    get new_event_event_registration_path(event)
+    assert_response :redirect
+  end
+end
