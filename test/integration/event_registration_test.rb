5ace2e0c919bfeb407785f03699791c4063f77ba
sunilkr <badboy16a@gmail.com>
2019-06-16 12:09:59 +0530

    Completed event registration test

--- test/integration/event_registration_test.rb
+++ test/integration/event_registration_test.rb
@@ -12,20 +12,26 @@ class EventRegistrationsTest < ActionDispatch::IntegrationTest
     get event_event_registrations_path(event)
     assert_response :ok
 
-    assert_raise (AbstractController::ActionNotFound) { get event_event_registration_path(event, registration) }
-    #assert_response :ok
+    assert_raise (AbstractController::ActionNotFound) { get event_event_registration_path(event, registration) }  #* Invalid operation
 
     get new_event_event_registration_path(event)
     assert_response :ok
 
-    assert_raise (AbstractController::ActionNotFound) { get edit_event_event_registration_path(event, registration) }
-    #assert_response :ok
+    assert_raise (AbstractController::ActionNotFound) { get edit_event_event_registration_path(event, registration) } #* Invalid operation
   end
   
-  test "Non logged user should be redirected" do
+  test "Unauthenticated user should be redirected" do
     event = events(:one)
     
     get new_event_event_registration_path(event)
-    assert_response :redirect
+    assert_response :redirect #TODO: check for redirect location.
+  end
+
+  test "Successfull registration redirects to event page" do
+    event = events(:one)
+    registration = event_registrations(:one)
+    
+    post event_event_registrations_path(event), event_registrations: registration 
+    assert_response :redirect #TODO: check for redirect location. 
   end
 end
