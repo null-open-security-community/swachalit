426ffba8f0170cc2135f9f7049e657c0ada8d7f2
abhisek <abhisek.datta@gmail.com>
2020-05-17 18:36:55 +0530

    Allow env to configure mailer host

--- config/application.rb
+++ config/application.rb
@@ -73,7 +73,7 @@ end
 if Rails.env.development?
     Rails.application.routes.default_url_options[:host] = '127.0.0.1'
 else
-    Rails.application.routes.default_url_options[:host] = 'null.co.in'
+    Rails.application.routes.default_url_options[:host] = ENV['APPLICATION_HOST'] || 'null.co.in'
 end
 
 require 'dotenv/load'
