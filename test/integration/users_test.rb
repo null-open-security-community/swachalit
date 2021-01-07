5009b122f287dabdb0d7a12dc0033fff3dbe3c39
Sunil Kumar <badboy16a@gmail.com>
2021-01-07 23:52:27 +0530

    Integration tests for /users/password path. (#65)
    * Addes some tests for users profile
    
    * Adding tp correct branch
    
    * Added 2 tests to users. TODO: password change
    
    * corrected tests
    
    * Skipping password change tests as this is handled by devise.

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -16,9 +16,29 @@ class UsersTest < ActionDispatch::IntegrationTest
     assert_response :ok
   end
 
-  test "Authenticated users should change password from profile page" do
+  test "Unauthenticated user can not access edit passsword page" do
+    get edit_user_password_path
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "Authenticated users should change password from profile page only" do
     sign_in users(:one)
     get new_user_password_path
     assert_redirected_to root_path
   end
+
+  test "Authenticated users should edit password from profile page only" do
+    sign_in users(:one)
+    get edit_user_password_path
+    assert_redirected_to root_path
+  end
+
+  #TODO: Do we need to test updates on /users/password?
+  # test "Authenticated users can edit password" do
+  #   sign_in users(:one)
+  #   user = users(:one)
+  #   put user_password_path, user:user
+  #   assert_response :ok
+  # end
+
 end
