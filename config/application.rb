ed39e106eb88d2805c2ebacae58f50e19921f179
abhisek <abhisek.datta@gmail.com>
2021-06-27 19:33:57 +0530

    Fix chapter overflow issue in ui

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module NullifyPlatform
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += Dir["#{config.root}/lib/api/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
